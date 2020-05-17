13daf0885f12e4cc2fd73728ed8f04000891d931
ebisLab <ebaiden@techie.com>
2020-05-17 17:37:23 -0400

    post page layout with grid template layout WIP

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { useParams, useRouteMatch} from "react-router-dom";
+import { useParams} from "react-router-dom";
+import '../App_post.css';
+import styled from 'styled-components'
 
 
 const Projects = ({data}) =>{
@@ -7,23 +9,93 @@ const Projects = ({data}) =>{
     const params = useParams();
         const info = data.find(item => item.id === Number(params.data));
         console.log(info,'info')
+
+const Intro = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+  &:before {
+    position: absolute;
+    bottom: 0;
+    content: "";
+    left: 0;
+    right: 0;
+    top: -50;
+    display: block;
+    background-image: url(${info && info.imgUrl});
+    background-size: cover;
+    transform-origin: center center 0;
+    transform:  translateZ(-1px) scale(2);
+    z-index: -1;
+    height: 110vh
+}
+`;
+
     return(
         <>
-        {info && (<div>
-        <h1>
-            This is the projects
-        </h1>
-        <section style={{display: 'inline-flex'}}>
-        <div 
-        style={{width: "200px", height:"200px", 
-        backgroundImage: `url(${info.imgUrl})`, backgroundSize: '100%'
-    }}
-        ></div>
-        <div>This is something about this compoennt</div>
-        <h3>{info.title}</h3>
+        {info && (  
+            
+            <div>
+<Intro className="intro"></Intro>
+
+<div></div>
+
+<div style={{background: "yellow"}}>
+<div className="content" style={{ background: "white"}}>
+    <div style={{display: "inline-flex", padding: "0 100px"}}>
+    <section style={{padding:"20px", width: "100%"}}><h2>{info.title}</h2></section>
+    
+    <section style={{padding:"20px"}}><p>
+{info.text}
+    </p>
+    </section>
+    </div>
+
+    </div>
+
+
+
+    <div className="content" style={{ background: "yellowgreen", display: "inline-grid", padding:0, gridTemplateColumns: 'repeat(2, 1fr)',}}>      
+            {info.imgs && info.imgs.map(im=>(
+            <div 
+            style={{
+                width: "50%", 
+                    backgroundImage: `url(${im})`,width:"100%", height: "30vw", backgroundSize: 'cover'            }}
+            ></div>
+           ))}      
+    </div>
+
+    <div className="content" style={{ background: "limegreen", display:"green"}}>
+        <div style={{display: "inline-flex", padding: "0 100px"}}>
+            <section style={{padding:"20px", width: "100%"}}><h2>{info.title}</h2></section>
+             <section style={{padding:"20px"}}><p>{info.text}</p></section>
+        </div>
+    </div>
+
+    <div className="content cellSection">      
+            {info.imgs && info.imgs.map(im=>(
+            <div 
+            className="cells"
+            style={{
+                background: "red",
+                width: "50%", 
+                    backgroundImage: `url(${im})`,width:"100%", height: "30vw", backgroundSize: 'cover'}}
+            ></div>
+            
+           ))}      
+    </div>
+
+
+
+
+<div style={{background: "yellow"}}><h2>{info.text}</h2></div>
+<div style={{background: "dodgerblue"}}><h2>{info.text}</h2></div>
+</div>
+
+</div>
         
-        </section>
-        </div>)}
+        
+        )}
         </>)
     
 }
--- src/mock.js
+++ src/mock.js
@@ -3,38 +3,54 @@ export const mock =[
         id:1,
     title: "Project Sample 1",
     category: "case studies",
-    imgUrl: "https://images.unsplash.com/photo-1526312426976-f4d754fa9bd6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1246&q=80"
+    imgUrl: "https://images.unsplash.com/photo-1526312426976-f4d754fa9bd6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1246&q=80",
+    text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis iure architecto ab eius repellat hic dolorum odit blanditiis possimus quos sunt quod a asperiores obcaecati numquam, nisi eveniet magnam impedit.",
+    imgs: ["https://images.unsplash.com/photo-1589561253898-768105ca91a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80", "https://images.unsplash.com/photo-1589550457981-00c39a2ede4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80","https://images.unsplash.com/photo-1589455719452-1ac812ae7a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"]
 },
 {
     id:2,
     title: "Project Sample 1",
     category: "case studies",
     imgUrl:'https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2767&q=80',
+    text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis iure architecto ab eius repellat hic dolorum odit blanditiis possimus quos sunt quod a asperiores obcaecati numquam, nisi eveniet magnam impedit.",
+    imgs: ["https://images.unsplash.com/photo-1589561253898-768105ca91a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80", "https://images.unsplash.com/photo-1589550457981-00c39a2ede4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80","https://images.unsplash.com/photo-1589455719452-1ac812ae7a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"]
+
     // imgUrl: "https://images.unsplash.com/photo-1585068247241-753a57d51cf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2536&q=80"
 },
 {
     id:3,
     title: "Project Sample 1",
     category: "experiments",
-    imgUrl: "https://images.unsplash.com/photo-1565306241695-8a5f675d6b57?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+    imgUrl: "https://images.unsplash.com/photo-1565306241695-8a5f675d6b57?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
+    text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis iure architecto ab eius repellat hic dolorum odit blanditiis possimus quos sunt quod a asperiores obcaecati numquam, nisi eveniet magnam impedit.",
+    imgs: ["https://images.unsplash.com/photo-1589561253898-768105ca91a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80", "https://images.unsplash.com/photo-1589550457981-00c39a2ede4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80","https://images.unsplash.com/photo-1589455719452-1ac812ae7a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"]
+
 },
 {
     id:4,
     title: "Project Sample 1",
     category: "illustrations",
-    imgUrl: "https://images.unsplash.com/photo-1585068247241-753a57d51cf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2536&q=80"
+    imgUrl: "https://images.unsplash.com/photo-1585068247241-753a57d51cf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2536&q=80",
+    text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis iure architecto ab eius repellat hic dolorum odit blanditiis possimus quos sunt quod a asperiores obcaecati numquam, nisi eveniet magnam impedit.",
+    imgs: ["https://images.unsplash.com/photo-1589561253898-768105ca91a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80", "https://images.unsplash.com/photo-1589550457981-00c39a2ede4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80","https://images.unsplash.com/photo-1589455719452-1ac812ae7a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"]
+
 },
 {
     id:8,
     title: "Project Sample 2",
     category: "app",
-    imgUrl: "https://images.unsplash.com/photo-1551651653-c5186a1fbba2?ixlib=rb-1.2.1&auto=format&fit=crop&w=1346&q=80"
+    imgUrl: "https://images.unsplash.com/photo-1551651653-c5186a1fbba2?ixlib=rb-1.2.1&auto=format&fit=crop&w=1346&q=80",
+    text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis iure architecto ab eius repellat hic dolorum odit blanditiis possimus quos sunt quod a asperiores obcaecati numquam, nisi eveniet magnam impedit.",
+    imgs: ["https://images.unsplash.com/photo-1589561253898-768105ca91a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80", "https://images.unsplash.com/photo-1589550457981-00c39a2ede4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80","https://images.unsplash.com/photo-1589455719452-1ac812ae7a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"]
+
 },
 {
     id:9,
     title: "Project Sample 8",
     category: "app",
-    imgUrl: "https://images.unsplash.com/photo-1588774580515-f9d4fd1aa1c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1420&q=80"
+    imgUrl: "https://images.unsplash.com/photo-1588774580515-f9d4fd1aa1c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1420&q=80",
+    text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis iure architecto ab eius repellat hic dolorum odit blanditiis possimus quos sunt quod a asperiores obcaecati numquam, nisi eveniet magnam impedit."
+
 },
 
 ]
